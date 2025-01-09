f3c5d09363cf022276c405c64f796de95d1a47ff
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-01-09 06:29:03 -0800

    some code

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-alert("Hello...")
+// var a = 3
+
+// a **= 8
+
+// alert(a)
+
+
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// **=	x **= y	x = x ** y
+
+// data types
+
+
+
+// string
+
+// number
+
+// boolean
+
+// undefiend
+
+// null
+
+// object
+
+// array
+
+// function
+
+// var a$_kjsfhskljhg2945782 = true
+
+// alert(a)
